3a07632cab1fa9582b8b2986ff294b8fe69a67b5
aaroncolombo <aaron.colombo@students.unibe.ch>
2022-10-26 11:16:14 +0200

    changed job script
    moved job scripts to scripts folder and added a job script to run 3 pipelines parallel

--- scripts/run_3mialab_piplines.sh
+++ scripts/run_3mialab_piplines.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
 # SLURM Settings
-#SBATCH --job-name="run_mialab_pipline"
+#SBATCH --job-name="run_3mialab_piplines"
 #SBATCH --time=24:00:00
+#SBATCH --ntasks=3
+#SBATCH --cpus-per-task=1
 #SBATCH --mem-per-cpu=128G
 #SBATCH --partition=epyc2
 #SBATCH --qos=job_epyc2
 #SBATCH --mail-user=aaron.colombo@students.unibe.ch
 #SBATCH --mail-type=ALL
+#SBATCH --chdir=../bin
 #SBATCH --output=%x_%j.out
 #SBATCH --error=%x_%j.err
 
@@ -19,4 +22,7 @@ eval "$(conda shell.bash hook)"
 conda activate mialab
 
 # Run your code
-srun python3 main.py
+srun --task=1 --cpus-per-task=1 python3 main.py &
+srun --task=1 --cpus-per-task=1 python3 main.py &
+srun --task=1 --cpus-per-task=1 python3 main.py &
+wait
