8a4c32befbe6537573de0c2d5b97178ccbb5f724
RafaelaPortmann <rafaela.portmann@gmx.ch>
2022-11-02 13:31:41 +0100

    function label_speration done

--- bin/label_seperation.py
+++ bin/label_seperation.py
@@ -52,32 +52,57 @@ except ImportError:
 
 LOADING_KEYS = [structure.BrainImageTypes.GroundTruth]  # the list of data we will load
 
-def label_separation(new_train_dir: str, new_test_dir: str, data_train_dir: str, data_test_dir: str, labels: list):
+#def label_separation(new_train_dir: str, new_test_dir: str, data_train_dir: str, data_test_dir: str, labels: list):
+def label_separation(new_train_dir: str, new_test_dir: str, data_train_dir: str, data_test_dir: str):
     """Brain label separation.
 
-        The main routine executes the medical image analysis pipeline:
+        The label seperation:
 
-            - Image loading
-            - Registration
-            - Pre-processing
-            - Feature extraction
-            - Decision forest classifier model building
-            - Segmentation using the decision forest classifier model on unseen images
-            - Post-processing of the segmentation
-            - Evaluation of the segmentation
+            - copies the train and test data if needed
+            - searches for the ground truth of the data
+            - in big structures the labels for the smalls are deleted
+            - in small structures the labels for the big are deleted
+            - Input labels will be delelted
     """
-    #  TODO: change the wording of the docstring
+    # labels for big and small seperation
+    labels = [1, 2]
 
-    #  TODO: if the new directories do not exist create them
-    #  TODO: copy the data directories in the new directories
+    # check if train folder exists
+    if not os.path.exists(new_train_dir):
+        os.makedirs(new_train_dir)
+
+    # check if test folder exists
+    if not os.path.exists(new_test_dir):
+        os.makedirs(new_test_dir)
+
+    # copy the data into the new directories
+    shutil.copytree(data_train_dir, new_train_dir, dirs_exist_ok=True)
+    shutil.copytree(data_test_dir, new_test_dir, dirs_exist_ok=True)
 
     #  TODO: Get the different ground truth maps of each patient
-    # crawler = futil.FileSystemDataCrawler(data_train_dir,
-    #                                       LOADING_KEYS,
-    #                                       futil.BrainImageFilePathGenerator(),
-    #                                       futil.DataDirectoryFilter())
+    crawler = futil.FileSystemDataCrawler(data_train_dir,
+                                          LOADING_KEYS,
+                                          futil.BrainImageFilePathGenerator(),
+                                          futil.DataDirectoryFilter())
+
+
+
 
     #  TODO: Delete the obsolete labels in each label map
+    for directory in os.listdir(new_train_dir):
+        img_dir = os.path.join(new_train_dir, directory)
+
+        if os.path.exists(os.path.join(img_dir,'labels_native.nii.gz')):
+            ground_truth = sitk.ReadImage(os.path.join(img_dir,'labels_native.nii.gz'))
+
+            ground_truth_array = sitk.GetArrayFromImage(ground_truth)
+            for label in labels:
+                ground_truth_array[ground_truth_array == label] = 0
+
+            ground_truth_labeled = sitk.GetImageFromArray(ground_truth_array)
+            ground_truth_labeled.CopyInformation(ground_truth)
+
+            sitk.WriteImage(ground_truth_labeled, os.path.join(img_dir,'labels_native.nii.gz'))
 
 
 if __name__ == "__main__":
@@ -90,14 +115,14 @@ if __name__ == "__main__":
     parser.add_argument(
         '--new_train_dir',
         type=str,
-        default=os.path.normpath(os.path.join(script_dir, './data/train_big/')),
+        default=os.path.normpath(os.path.join(script_dir, '../data/train_big/')),
         help='Directory for separated train labels.'
     )
 
     parser.add_argument(
         '--new_test_dir',
         type=str,
-        default=os.path.normpath(os.path.join(script_dir, './data/test_big/')),
+        default=os.path.normpath(os.path.join(script_dir, '../data/test_big/')),
         help='Directory for separated train labels.'
     )
 
@@ -109,6 +134,13 @@ if __name__ == "__main__":
     )
 
     parser.add_argument(
+        '--data_test_dir',
+        type=str,
+        default=os.path.normpath(os.path.join(script_dir, '../data/test/')),
+        help='Directory with test data.'
+    )
+
+    parser.add_argument(
         '--labels',
         type=list,
         default=[1, 2],
@@ -116,4 +148,4 @@ if __name__ == "__main__":
     )
 
     args = parser.parse_args()
-    label_separation(args.new_train_dir, args.new_test_dir, args.data_train_dir, args.data_test_dir, )
+    label_separation(args.new_train_dir, args.new_test_dir, args.data_train_dir, args.data_test_dir)
