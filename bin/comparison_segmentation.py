a3065d9252d7ff3446c1333aa70634310cb92d53
Zahira Mercado <zahira.mercado@gmx.ch>
2022-11-02 13:35:44 +0100

    Update comparison_segmentation.py
    function csv merge done
    missing: correct paths

--- bin/comparison_segmentation.py
+++ bin/comparison_segmentation.py
@@ -1,24 +1,41 @@
 import os.path
 import glob
-import panda as pd
+import pandas as pd
 
 
 def combine():
     script_dir = os.path.dirname(__file__)
-    dir_all = os.path.join(script_dir, './mia-result/2022-10-10-21-12-42/results.csv') #to be defined
-    dir_big = os.path.join(script_dir, './mia-result/2022-10-10-21-12-42/results.csv') #to be defined
-    dir_small = os.path.join(script_dir, './mia-result/2022-10-10-21-12-42/results.csv') #to be defined
-    end_dir = os.path.join(script_dir, './mia-results/')
+    # test path
+    #script_dir = 'C:/Users/zahir/Documents/Studies/MSc_Bern/3_Semester/MIALab/bin/'
+    dir_all = os.path.join(script_dir, './mia-result/2022-10-10-21-12-42/results.csv')  # to be defined
+    dir_big = os.path.join(script_dir, './mia-result/2022-10-17-16-14-02_Big/results.csv')  # to be defined
+    dir_small = os.path.join(script_dir, './mia-result/2022-10-17-16-14-02_Small/results.csv')  # to be defined
+    end_dir = os.path.join(script_dir, './combine_results/')  # to be defined
+    if not os.path.exists(end_dir):
+        os.mkdir(end_dir)
 
-    data =[]
-    dataA = pd.read_csv(dir_all, sep=";")
-    dataB = pd.read_csv(dir_big, sep=";")
-    dataS = pd.read_csv(dir_small, sep=";")
 
-    data.append(dataA)
-    data.append(dataB)
-    data.append(dataS)
+    dfA = pd.DataFrame(pd.read_csv(dir_all, sep=";"))
+    dfB = pd.DataFrame(pd.read_csv(dir_big, sep=";"))
+    dfS = pd.DataFrame(pd.read_csv(dir_small, sep=";"))
 
-    data_merged = pd.concat(data, ignore_index=True)
+    data_merged_right = pd.merge_ordered(dfB, dfS, how='outer')
+    bs_column_names = ['SUBJECT BS', 'LABEL BS', 'DICE BS', 'HDRFDST BS']
+    data_merged_right.columns = bs_column_names
 
-    data_merged.to_csv(end_dir+ 'results_comnbined.csv', index = False)
+
+    data_merged_right.to_csv(end_dir + 'Big_Small_Merged.csv', index=True)
+
+
+    df_merged = pd.concat([dfA, data_merged_right], axis = 1)
+    df_merged_smaller = df_merged.drop(axis=1, columns=['SUBJECT BS', 'LABEL BS'])
+
+    df_merged_smaller.to_csv(end_dir + 'all_right.csv', index=True)
+
+
+def main ():
+    combine()
+
+# for testing
+if __name__ == '__main__':
+    main()
